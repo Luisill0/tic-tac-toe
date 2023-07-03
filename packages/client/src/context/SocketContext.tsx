682172ecfc30202fd0eb3f2606633a57660b74ba
luis <47650944+Luisill0@users.noreply.github.com>
2023-07-03 08:35:28 -0600

    sockets in context

--- packages/client/src/context/SocketContext.tsx
+++ packages/client/src/context/SocketContext.tsx
@@ -1,22 +1,57 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useContext, useState } from "react";
 import { io } from 'socket.io-client';
 
-import { SocketContextProps } from "@types";
+import { SocketContextProps, UserContextProps } from "@types";
+import { UserContext } from "context";
+import { BoardType, GameMove } from "@tic-tac-toe/shared";
+import { winState } from "@helpers/board";
+
+export const SocketContext = React.createContext<SocketContextProps | null>(null);
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? window.location : 'http://localhost:8000';
 
-export const SocketContext = React.createContext<SocketContextProps | null>(null);
-
 export const SocketContextProvider = ({children}: PropsWithChildren): JSX.Element => {
-    const socket = io(URL);
+    const { board, updateBoard, togglePlayer } = useContext(UserContext) as UserContextProps;
+    const [ socket ] = useState(() => io(URL, {autoConnect: false}));
+
+    const connect = () => {
+        if(socket.connected) return;
+        socket.connect();
+    }
 
     const sayHello = () => {
         socket.emit('hello', 'test');
     }
 
+    const emitMove = (move: GameMove) => {
+        console.log('i want to emit a move', move);
+        socket.emit('move-sent', move);
+    }
+
+    const moveMade = (move: GameMove) => {
+        console.log('i client received a move ', move);
+        let tile = move.position;
+        let player = move.player;
+
+        let newBoard = [...board] as BoardType;
+        newBoard[tile] = player;
+
+        console.log('i want to update the board', newBoard);
+        
+        let newP = winState(newBoard) ? player : undefined;
+
+        console.log('newP: ',newP);
+        togglePlayer(newP);
+        updateBoard(newBoard);
+    }
+
+    socket.on('move-made', moveMade);
+
     const provider = {
-        sayHello
+        connect,
+        sayHello,
+        emitMove
     }
 
     return (
