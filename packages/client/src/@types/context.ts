45975f434137cc1a03738c44638a277d430423ac
luis <47650944+Luisill0@users.noreply.github.com>
2023-07-23 18:57:38 -0600

    edit context interfaces

--- packages/client/src/@types/context.ts
+++ packages/client/src/@types/context.ts
@@ -1,10 +1,28 @@
-import { BoardType, Player } from "@tic-tac-toe/shared";
-import { WindowDimensions } from "./window";
+import { BoardType, GameMove, Player } from "@tic-tac-toe/shared";
+import { WindowDimensions } from '@types';
 
 export type UserContextProps = {
     windowSize: WindowDimensions;
+}
+
+export type BoardContextProps = {
     board: BoardType;
     updateBoard: (newBoard: BoardType) => void;
     currentPlayer: Player;
     togglePlayer: (newPlayer?: Player | undefined) => void;
+    myPlayer: Player;
+    updateMyPlayer: (myPlayer: Player) => void;
+    myTurn: boolean,
+    updateMyTurn: (myTurn: boolean) => void;
+    resetBoard: () => void;
+}
+
+export type SocketContextProps = {
+    connect: () => void;
+    createRoom: (roomId: string) => void;
+    joinRoom: (roomId: string) => void;
+    sayHello: () => void;
+    emitMove: (move: GameMove) => void;
+    currentSocketRoom: string | null;
+    resetSocketRoom: () => void;
 }
