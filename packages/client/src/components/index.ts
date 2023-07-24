9a261e0beb549ed6b55abec37f61bb82ccf942be
luis <47650944+Luisill0@users.noreply.github.com>
2023-07-23 18:56:10 -0600

    components index

--- packages/client/src/components/index.ts
+++ packages/client/src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './board';
 export { default as InfoIndicator } from './InfoIndicator';
+export { default as JoinGameModal } from './JoinGameModal';
+export { default as TextWithCopy } from './TextWithCopy';
